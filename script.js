f267734f4cc2383705cf19a18461e7c85a3ec65b
abaevashahizat-dot <abaeva.shahizat@mail.ru>
2026-09-23 09:53:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-console.log("AI-Ustaz SCRIPT.JS ЗАГРУЖЕН");
 
-document.querySelectorAll(".card").forEach(function(card) {
-
-card.addEventListener("click", function() {
-alert("РАБОТАЕТ: " + card.dataset.tool);
-
-});
-
-});
